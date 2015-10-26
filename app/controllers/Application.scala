97c6358700e6c509d540c41f35d6bcaf7a0eb959
Kevin Adistambha <kevinadi@gmail.com>
2015-10-26 22:45:01 +1100

    Hello World

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,10 +7,23 @@ import play.api.Play.current
 
 import play.api.db._
 
+import javax.measure.unit.SI.KILOGRAM
+import javax.measure.quantity.Mass
+import org.jscience.physics.model.RelativisticModel
+import org.jscience.physics.amount.Amount
+
 object Application extends Controller {
 
+  // def index = Action {
+  //   Ok(views.html.index(null))
+  // }
+
   def index = Action {
-    Ok(views.html.index(null))
+    RelativisticModel.select()
+    val m = Amount.valueOf("12 GeV").to(KILOGRAM)
+    val testRelativity = s"E=mc^2: 12 GeV = $m"
+    // Ok(views.html.index(testRelativity))
+    Ok(views.html.index("Hello World"))
   }
 
   def db = Action {
